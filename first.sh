0e16fbfc07df8b58d2c88bd666c24bf75cf90b46
Sai Royal <sairoyal@Sais-Air.domain.name>
2024-08-28 21:43:16 +0530

    first push

--- first.sh
+++ first.sh
@@ -9,3 +9,9 @@ PERSON2=loka
 
 echo "$PERSON2 :: Hi $PERSON1"
 echo "$PERSON1 :: Hello $PERSON2, How are you?"
+
+FRUITS=("banana","apple","jack")
+echo "printing first fruit: ${FRUITS[0]}"
+echo "printing first fruit: ${FRUITS[1]}"
+echo "printing first fruit: ${FRUITS[2]}"
+echo "printing first fruit: ${FRUITS[@]}"
