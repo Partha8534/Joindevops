e755ea8ce687ce523ecd1eec0e78d622e2b9e578
Sai Royal <sairoyal@Sais-Air.domain.name>
2024-08-28 21:47:27 +0530

    first push

--- first.sh
+++ first.sh
@@ -10,9 +10,11 @@ PERSON2=loka
 echo "$PERSON2 :: Hi $PERSON1"
 echo "$PERSON1 :: Hello $PERSON2, How are you?"
 
-FRUITS=("banana","apple","jack")
 
-echo "printing first fruit: ${FRUITS[0]}"
-echo "printing second fruit: ${FRUITS[1]}"
-echo "printing third fruit: ${FRUITS[2]}"
-echo "printing all fruit: ${FRUITS[@]}"
+RUITS=("APPLE" "KIWI" "ORANGE") #Array
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "First fruit is: ${FRUITS[@]}"
