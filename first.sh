426da971ac729c1aa05c2ab69f38a99441888fc1
Sai Royal <sairoyal@Sais-Air.domain.name>
2024-08-28 21:40:22 +0530

    first push

--- first.sh
+++ first.sh
@@ -7,5 +7,5 @@ echo "Today's date: $DATE"
 PERSON1=sai
 PERSON2=loka
 
-echo "Hi $PERSON1"
-echo "Hello $PERSON2, How are you?"
+echo "$PERSON2 :: Hi $PERSON1"
+echo "$PERSON1 :: Hello $PERSON2, How are you?"
