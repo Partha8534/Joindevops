9e5e66ad2fd8e67c66d24b5a28cd6e2582910d51
partha <partha8534@gmail.com>
2024-08-29 21:57:37 +0530

    My commit

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,6 +5,22 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "Run script as Sudo User"
-else
-echo "Run script Success"
+exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+echo "Git installation Not success"
+exit 1
+else 
+echo "Git instalation success"
+fi
+else 
+echo "Git already insttalled"
 fi
